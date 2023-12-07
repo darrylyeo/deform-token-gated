74e7fe18b8848725dd222d3976b310f63ca77582
Darryl Yeo <dev@darryl-yeo.com>
2023-12-07 12:26:59 -0800

    Set up app layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	
+</script>
+
+
+<header class="row">
+	<h1>TokenGated</h1>
+
+	<div class="row">
+		<button>Connect</button>
+	</div>
+</header>
+
+
+<style>
+	:global(*) {
+		box-sizing: border-box;
+		margin: 0;
+		padding: 0;
+		font-family: sans-serif;
+	}
+
+	:global(body) {
+		background-color: palegoldenrod;
+	}
+
+	header {
+		background-color: rgba(0, 0, 0, 0.1);
+		backdrop-filter: blur(10px);
+		padding: 2rem;
+	}
+
+	button {
+		appearance: none;
+		border: none;
+		border-radius: 10px;
+		padding: 0.5rem;
+		line-height: 1;
+	}
+
+	.row {
+		display: flex;
+		justify-content: space-between;
+		align-items: center;
+		gap: 1em;
+	}
+</style>
