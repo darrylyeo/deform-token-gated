9159bc523b3ce60a60a3a4385308a8d59c93fc08
Darryl Yeo <dev@darryl-yeo.com>
2023-12-08 14:17:59 -0800

    Set up global CSS

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -75,35 +75,11 @@
 
 
 <style>
-	:global(*) {
-		box-sizing: border-box;
-		margin: 0;
-		padding: 0;
-		font-family: sans-serif;
-	}
-
-	:global(body) {
-		background-color: palegoldenrod;
-	}
+	@import '../styles.css';
 
 	header {
 		background-color: rgba(0, 0, 0, 0.1);
 		backdrop-filter: blur(10px);
 		padding: 2rem;
-	}
-
-	button {
-		appearance: none;
-		border: none;
-		border-radius: 10px;
-		padding: 0.5rem;
-		line-height: 1;
-	}
-
-	.row {
-		display: flex;
-		justify-content: space-between;
-		align-items: center;
-		gap: 1em;
 	}
 </style>
