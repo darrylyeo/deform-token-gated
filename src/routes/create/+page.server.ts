0ef206a7aa5525e6921f0f9ab427743475f74a61
Darryl Yeo <dev@darryl-yeo.com>
2023-12-09 10:47:51 -0800

    "create" endpoint: insert objects into database

--- src/routes/create/+page.server.ts
+++ src/routes/create/+page.server.ts
@@ -2,6 +2,12 @@ import type { AccessConditionType, SerializedAccessCondition } from '$lib/Access
 import type { Address } from 'viem'
 import type { Actions } from './$types'
 
+import * as edgedb from 'edgedb'
+import e from '../../database/dbschema/edgeql-js'
+
+const client = edgedb.createClient()
+
+
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData()
@@ -10,9 +16,9 @@ export const actions = {
 		const title = data.get('title') as unknown as string
 		const content = data.get('content') as unknown as string
 		const conditionTypes = data.getAll('conditionType') as unknown as AccessConditionType[]
-		const chainIds = data.getAll('chainId') as unknown as number[]
+		const chainIds = data.getAll('chainId').map(Number)
 		const contractAddresses = data.getAll('contractAddress') as unknown as Address[]
-		const tokenIds = data.getAll('tokenId') as unknown as number[]
+		const tokenIds = data.getAll('tokenId').map(Number)
 
 		const conditions: SerializedAccessCondition[] = conditionTypes.map((type, i) => ({
 			type,
@@ -27,9 +33,45 @@ export const actions = {
 			content,
 			conditions,
 		})
+		
+		const result = await (
+			e.params({
+				creatorAddress: e.Address,
+				title: e.str,
+				content: e.str,
+				conditions: e.json,
+			}, ({
+				creatorAddress,
+				title,
+				content,
+				conditions,
+			}) => (
+				e.insert(e.Page, {
+					creatorAddress: e.cast(e.Address, creatorAddress),
+					title,
+					content,
+					conditions: e.for(e.json_array_unpack(conditions), (condition) => (
+						e.insert(e.AccessCondition, {
+							conditionType: e.cast(e.AccessConditionType, condition.type),
+							chainId: e.cast(e.int16, condition.chainId || null),
+							contractAddress: e.cast(e.Address, condition.contractAddress || null),
+							tokenId: e.cast(e.bigint, condition.tokenId || null),
+						})
+					))
+				})
+			))
+		)
+			.run(client, {
+				creatorAddress,
+				title,
+				content,
+				conditions,
+			})
+
+		console.log({result})
 
 		return {
-			success: true,
+			result,
 		}
 	},
 } satisfies Actions
