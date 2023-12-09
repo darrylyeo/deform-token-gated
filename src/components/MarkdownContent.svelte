41ef010eb0bd514870cf7675bf5eeef0062ddac0
Darryl Yeo <dev@darryl-yeo.com>
2023-12-09 14:20:01 -0800

    <MarkdownContent>: display markdown content

---
+++ src/components/MarkdownContent.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import { Carta, CartaViewer } from 'carta-md'
+
+
+	let {
+		value,
+	} = $props<{
+		value: string,
+	}>()
+</script>
+
+
+<CartaViewer
+	carta={new Carta({
+		disableIcons: true,
+	})}
+	{value}
+/>
