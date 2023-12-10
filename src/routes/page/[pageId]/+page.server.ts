3d20745d3c53746d6d5dc377ff627af1b885cdb2
Darryl Yeo <dev@darryl-yeo.com>
2023-12-09 20:48:03 -0800

    Page: implement access condition enforcing logic

--- src/routes/page/[pageId]/+page.server.ts
+++ src/routes/page/[pageId]/+page.server.ts
@@ -1,5 +1,8 @@
 import type { PageServerLoad } from './$types'
 import { client, e } from '../../../database/client'
+import { AccessConditionType } from '$lib/AccessCondition.svelte'
+import { isNftContractHolder, isNftTokenHolder } from '../../../lib/alchemy'
+import { error } from '@sveltejs/kit'
 
 export const load: PageServerLoad = async ({ params: { pageId } }) => {
 	const page = await (
@@ -26,19 +29,47 @@ export const load: PageServerLoad = async ({ params: { pageId } }) => {
 	)
 		.run(client, { pageId: pageId! })
 
-	console.log({ page })
+	const userAddress = ''
 
-	if (!page) {
-		return {
-			status: 404,
-			error: new Error(`Page ${pageId} not found`),
-		};
-	}
+	if(!page)
+		throw error(404, `Page ${pageId} not found`)
 
-	return {
-		status: 200,
-		body: {
-			page,
-		},
-	};
+	const canAccess = (await Promise.all(
+		page.conditions
+			.map(async condition => {
+				switch(condition.conditionType) {
+					case AccessConditionType.None:
+						return true
+
+					case AccessConditionType.OwnsNftInCollection: {
+						return await isNftContractHolder({
+							userAddress,
+							chainId: condition.chainId!,
+							contractAddress: condition.contractAddress!
+						})
+					}
+
+					case AccessConditionType.OwnsSingleNft: {
+						return await isNftTokenHolder({
+							userAddress,
+							chainId: condition.chainId!,
+							contractAddress: condition.contractAddress!,
+							tokenId: condition.tokenId!,
+						})
+
+						// const {
+						// 	isHolderOfContract
+						// } = await fetch(`https://eth-mainnet.g.alchemy.com/nft/v3/${env.ALCHEMY_API_KEY}/getNFTsForOwner?owner=${userAddress}&contractAddresses=[${condition.contractAddress}]`)
+						// 	.then(response => response.json())
+
+						// return isHolderOfContract
+					}
+				}
+			})
+	)).every(Boolean)
+
+	if(!canAccess)
+		throw error(403, `Address ${userAddress} does not have access to page ${pageId}`)
+
+	return page
 }
