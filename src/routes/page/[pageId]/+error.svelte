924e40105b21611ca20144f551267dcdfc3d4f82
Darryl Yeo <dev@darryl-yeo.com>
2023-12-10 21:46:34 -0800

    Customize errror page layout

---
+++ src/routes/page/[pageId]/+error.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	// Context
+	import { page } from '$app/stores'
+	console.log($page.error)
+</script>
+
+
+<article class="column">
+	<h1>{$page.status}</h1>
+
+	{#if $page.error}
+		<div class="card column">
+			{$page.error.message}
+		</div>
+	{/if}
+</article>
+
+
+<style>
+	h1 {
+		text-align: center;
+		font-size: 2em;
+	}
+
+	article {
+		gap: 2em;
+		padding: 1em;
+	}
+
+	.card {
+		padding: 2em;
+		width: min(100%, 50em);
+		line-height: 1.5em;
+		margin: auto;
+	}
+
+	footer {
+		opacity: 0.5;
+		text-align: center;
+	}
+</style>
